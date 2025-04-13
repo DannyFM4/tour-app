c7b4d9651bc955802956c78fc0a1af4d127f3599
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-04-13 13:48:17 -0400

    TourCard - Adding Comments

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -6,17 +6,21 @@ const TourCard = ({id, name, info, price, image, onRemove}) => {
 
     return (
         <article className="tour-card">
+            {/* Laysout the tour card information */}
             <h3>{name}</h3>
             <h5 className="tour-price">{price}</h5>
             <img src={image} alt={name} className="tour-image"/>
 
             <p>
+                {/*Will how more information or can show less*/}
                 {readMore ? info : `${info.substring(0, 80)}...`}
                 <button onClick={() => setReadMore(!readMore)} className="read-more-btn">
+                    {/* Button Text */}
                     {readMore ? 'Show Less' : 'Read More'}
                 </button>
             </p>
 
+            {/* Button to remove the tour card from the list */}
             <button className="btn-remove" onClick={() => onRemove(id)}>Not Interested</button>
 
 
